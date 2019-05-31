4c8906629615b29af8cd7cab20def9bc9dd8b0ae
HeroBrine1st <herobrine1.videos@gmail.com>
2019-05-31 12:51:48 +0300

    Темпмуты, должно работать. В случае чего - реверт и вперед

--- kaizen_modules/moderation.py
+++ kaizen_modules/moderation.py
@@ -1,3 +1,5 @@
+import time
+
 import discord
 
 import kaizen85modules
@@ -32,12 +34,33 @@ class MutedUser:
         self.guild_id = guild_id
 
 
+class TempMutedUser(MutedUser):
+    duration = 0
+
+    def __init__(self, user_id, guild_id, deadline):
+        super().__init__(user_id, guild_id)
+        self.deadline = deadline
+
+
+async def background_task(bot):
+    while True:
+        for muted_user in bot.module_handler.params["moderation_mutes"]:
+            if isinstance(muted_user, TempMutedUser):
+                if muted_user.deadline < time.time():
+                    role = await bot.get_guild(muted_user.guild_id).get_role(MUTED_ROLE_ID)
+                    user: discord.Member = await bot.get_user(muted_user.user_id)
+                    await user.remove_roles(role, reason="Unmute: timed out")
+                    bot.module_handler.params["moderation_mutes"].remove(muted_user)
+                    bot.module_handler.save_params()
+
+
 class Module(kaizen85modules.ModuleHandler.Module):
     name = "Moderation"
     desc = "Полезные команды для модерации"
 
     async def run(self, bot: kaizen85modules.KaizenBot):
         bot.module_handler.add_param("moderation_mutes", [])
+        bot.module_handler.add_background_task(background_task(bot))
 
         class CommandPurge(bot.module_handler.Command):
             name = "purge"
@@ -61,8 +84,8 @@ class Module(kaizen85modules.ModuleHandler.Module):
                     return True if len(message.mentions) < 1 else msg.author == message.mentions[0]
 
                 switch_kgbmode = False
-                if "kgbmode_enabled" in bot.module_handler.params and bot.module_handler.params[
-                    "kgbmode_enabled"] is True:
+                if "kgbmode_enabled" in bot.module_handler.params \
+                        and bot.module_handler.params["kgbmode_enabled"] is True:
                     bot.module_handler.params["kgbmode_enabled"] = False
                     switch_kgbmode = True
 
@@ -144,6 +167,62 @@ class Module(kaizen85modules.ModuleHandler.Module):
 
                 return True
 
+        class CommandTempMute(bot.module_handler.Command):
+            name = "tempmute"
+            desc = "Замутить пользователя навсегда."
+            args = "<@пользователь> [длительность в секундах] [причина]"
+            permissions = ["manage_roles"]
+
+            async def run(self, message: discord.Message, args, keys):
+                if len(message.mentions) < 1:
+                    return False
+                if message.mentions[0] == bot.user:
+                    await bot.send_error_embed(message.channel, "Вы не можете замутить бота!")
+                    return True
+
+                if message.mentions[0] == message.author:
+                    await bot.send_error_embed(message.channel, "Вы не можете замутить самого себя!")
+                    return True
+
+                role = message.guild.get_role(MUTED_ROLE_ID)
+
+                if role is None:
+                    await bot.send_error_embed(message.channel, "Роль с ID %s не найдена!" % MUTED_ROLE_ID)
+                    return True
+
+                if role in message.mentions[0].roles:
+                    await bot.send_error_embed(message.channel, "Этот пользователь уже в муте!")
+                    return True
+
+                if message.author.guild_permissions < message.mentions[0].guild_permissions:
+                    await bot.send_error_embed(message.channel, "Вы не можете замутить этого пользователя.")
+                    return True
+
+                reason = "Плохое поведение"
+                if len(args) < 2:
+                    return False
+                duration = int(args[1])
+                deadline = time.time() + duration
+                if len(args) > 2:
+                    reason = " ".join(args[2:])
+
+                await message.mentions[0].add_roles(role, reason=reason)
+
+                await bot.send_error_embed(bot.get_channel(MODLOG_CHANNEL_ID),
+                                           "%s был заткнут %s по причине \"%s\"." % (
+                                               message.mentions[0].mention, message.author.mention, reason),
+                                           "Наказания")
+
+                for user in bot.module_handler.params["moderation_mutes"]:
+                    if user.user_id == message.mentions[0].id:
+                        return True
+
+                muted_user = TempMutedUser(message.mentions[0].id, message.author.guild.id, deadline)
+                bot.module_handler.params["moderation_mutes"].append(muted_user)
+                bot.module_handler.save_params()
+
+                return True
+
         class CommandUnmute(bot.module_handler.Command):
             name = "unmute"
             desc = "Снять мут с пользователя."
@@ -178,6 +257,7 @@ class Module(kaizen85modules.ModuleHandler.Module):
 
         bot.module_handler.add_command(CommandPurge(), self)
         bot.module_handler.add_command(CommandMute(), self)
+        bot.module_handler.add_command(CommandTempMute(), self)
         bot.module_handler.add_command(CommandUnmute(), self)
 
     async def on_member_join(self, member: discord.Member, bot):
