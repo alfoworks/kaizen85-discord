813b9893c397ea3a3eaa790933ba81bb1dda7b60
Rob <rob.bog@list.ru>
2019-12-09 00:33:21 +0300

    Кастомное сообщение при входе юзверя (kgb), включает в себя отправку имени пригласившего

--- kaizen_modules/kgb.py
+++ kaizen_modules/kgb.py
@@ -1,6 +1,21 @@
+import asyncio
+
 import discord
 
 import kaizen85modules
+
+client: kaizen85modules.KaizenBot = None
+invites = {}
+
+
+async def invite_update_task():
+    while True:
+        for guild in client.guilds:
+            new_invites = await guild.invites()
+            if guild.id not in invites or invites[guild.id] != new_invites:
+                invites[guild.id] = new_invites
+
+        await asyncio.sleep(2)
 
 
 class Module(kaizen85modules.ModuleHandler.Module):
@@ -10,13 +25,41 @@ class Module(kaizen85modules.ModuleHandler.Module):
     desc = "Секрет КГБ!"
 
     async def run(self, bot: kaizen85modules.KaizenBot):
+        global client
         bot.module_handler.add_param("kgbmode_enabled", True)
+        bot.module_handler.add_param("custom_join_message", True)
+
+        client = bot
+        bot.module_handler.add_background_task(self, invite_update_task())
 
     async def on_member_remove(self, member: discord.Member, bot):
         if member.guild.system_channel:
             await bot.send_error_embed(member.guild.system_channel,
                                        "%s (%s) вышел с сервера." % (member.mention, member),
                                        "Инфо")
+
+    async def on_member_join(self, member: discord.Member, bot):
+        if member.guild.system_channel_flags.join_notifications:
+            return
+
+        inviter: discord.User
+
+        if member.guild.id in invites:
+            invites_unpacked = {}
+
+            for invite in invites[member.guild.id]:
+                invites_unpacked[invite.code] = invite
+
+            for invite in await member.guild.invites():
+                if invites_unpacked[invite.code].uses != invite.uses:
+                    inviter = invite.inviter
+                    break
+
+        await bot.send_ok_embed(member.guild.system_channel,
+                                "%s вошёл на сервер (пригласил: %s)." % (
+                                    member.mention, inviter if inviter else "неизвестно"), title="Инфо")
+
+        invites[member.guild.id] = await member.guild.invites()
 
     async def on_message_delete(self, message: discord.Message, bot: kaizen85modules.KaizenBot):
         if message.id in self.kgb_messages:
