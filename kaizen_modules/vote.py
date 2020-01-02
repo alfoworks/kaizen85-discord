459ffc183971256b99ab9e36b03ee0f254137776
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2020-01-02 19:13:12 +0300

    Пофикшено

--- kaizen_modules/vote.py
+++ kaizen_modules/vote.py
@@ -13,11 +13,11 @@ class VoteCommand(kaizen85modules.ModuleHandler.Command):
 
     def __init__(self, bot: kaizen85modules.KaizenBot):
         guild: discord.Guild = bot.get_guild(bot.GLOBAL_GUILD_LOCK)
-        self.cross: discord.Emoji = get(guild.emojis, name="cross-1")
+        self.cross: discord.Emoji = get(guild.emojis, name="cross")
         self.bot = bot
 
-    def run(self, message: discord.Message, args: str, keys: List[str]) -> bool:
-        message.add_reaction(self.cross)
+    async def run(self, message: discord.Message, args: str, keys: List[str]) -> bool:
+        await message.add_reaction(self.cross)
         return True
 
 
@@ -25,5 +25,5 @@ class Module(kaizen85modules.ModuleHandler.Module):
     name = "Vote"
     desc = "Позволяет автоматически ставить эмодзи на сообщение (крестик и галочка)"
 
-    def run(self, bot: kaizen85modules.KaizenBot):
+    async def run(self, bot: kaizen85modules.KaizenBot):
         bot.module_handler.add_command(VoteCommand(bot), self)
